1ca292b19ab22af04e56ea3b4676b49af5adfa91
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-05-29 23:18:07 +0300

    Save

---
+++ HabitApp/AddHabitViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddHabitViewController.swift
+//  HabitApp
+//
+//  Created by Олейник Богдан on 29.05.2022.
+//
+
+import UIKit
+
+class AddHabitViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+    }
+
+}
+
+extension AddHabitViewController: {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+}
--- HabitApp/ViewController.swift
+++ HabitApp/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupNavigationBar()
     }
-
+    
+    private func setupNavigationBar() {
+        title = "List"
+        
+        let navigationBarApperance = UINavigationBarAppearance()
+        
+        navigationBarApperance.backgroundColor = UIColor(#colorLiteral(red: 0.6810721159, green: 0.7772024274, blue: 1, alpha: 1))
+        
+        navigationBarApperance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addHabit)
+        )
+        
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.standardAppearance = navigationBarApperance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarApperance
+        
+    }
+    
+    @objc private func addHabit() {
+        let addHabitVC = AddHabitViewController()
+        present(addHabitVC, animated: true)
+    }
 
 }
 
