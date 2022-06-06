a6c14ea317eed8d1c2b9ef8d12ffd8f194e7806d
Bogdan <96473517+BogdanOleinik@users.noreply.github.com>
2022-06-06 16:21:56 +0300

    CoreData

---
+++ HabitApp/Services/StorageManager.swift
@@ -0,0 +1,74 @@
+//
+//  StorageManager.swift
+//  HabitApp
+//
+//  Created by Олейник Богдан on 06.06.2022.
+//
+
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "HabitApp")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    private let viewContext: NSManagedObjectContext
+    
+    private init() {
+        viewContext = persistentContainer.viewContext
+    }
+    
+    // MARK: - Public Method
+    func fetchData(completion: (Result<[UserTasks], Error>) -> Void) {
+        let fetchRequest = UserTasks.fetchRequest()
+        
+        do {
+            let habits = try viewContext.fetch(fetchRequest)
+            completion(.success(habits))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    // Sava data
+    func save(_ taskName: String, completion: (UserTasks) -> Void) {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "UserTasks", in: viewContext) else { return }
+        guard let task = NSManagedObject(entity: entityDescription, insertInto: viewContext) as? UserTasks else { return }
+        task.taskName = taskName
+        completion(task)
+        saveContext()
+    }
+    
+    func edit(_ task: UserTasks, newName: String) {
+        task.taskName = newName
+        saveContext()
+    }
+    
+    func delete(_ task: UserTasks) {
+        viewContext.delete(task)
+        saveContext()
+    }
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
+
